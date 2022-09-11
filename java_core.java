8f16c433707f508a45e964ae0e3e73d3a54f1cf7
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-11 15:00:58 +0530

    FLIP COIN TO GET PERCENTAGE OF HEAD OR TAIL

--- java_core.java
+++ java_core.java
@@ -3,6 +3,29 @@ public class java_core
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "WELCOME TO BASICS CORE JAVA PROGRAM" );
+        
+        
+        System.out.println( "COIN FLIP" );
+        double T=0;
+		double H=0;
+		for(int i=1;i<=10;i++)
+		{
+			double Flip=Math.random();
+			System.out.println("coin "+Flip);
+			if(Flip < 0.5) 
+			{
+				T++;
+			}
+			else 
+			{
+				H++;
+		    }
+		}
+		System.out.println("head"+H);
+		double percentageheads=(H/10)*100;
+		double percentagetails=(T/10)*100;
+		System.out.println("heads percentage is "+percentageheads);
+		System.out.println("tails percentage is "+percentagetails);
     }
 }
